ebe83a86efd2b12ac375f65e21b3bd538ff44228
eziox <ezioxxk@gmail.com>
2025-01-18 20:14:25 +0100

    init

--- src/routes/think/+server.ts
+++ src/routes/think/+server.ts
@@ -2,7 +2,12 @@ import type {RequestEvent} from "@sveltejs/kit";
 import {hypothesesTable} from "$lib/server/db/schema";
 
 export const POST = async (event:RequestEvent)=> {
-    const db = event.locals.db;
-    const hypo = db.select().from(hypothesesTable).all();
-    return {hypo};
+    try {
+        const db = event.locals.db;
+        const hypo = db.select().from(hypothesesTable).all();
+        return {hypo};
+    } catch (error) {
+        console.log(error);
+        return {error};
+    }
 }
