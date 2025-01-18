46bec52a9e86158e51cd0ad279f7c4f94f616f88
eziox <ezioxxk@gmail.com>
2025-01-18 20:10:05 +0100

    init

--- src/lib/integrations/openai/llmTest.test.ts
+++ src/lib/integrations/openai/llmTest.test.ts
@@ -1,5 +1,6 @@
 import {test, expect} from "vitest"
 import {generateAlternativeHypotheses} from "$lib/integrations/openai/think";
+import ky from "ky";
 
 test.only("test hypo", async () => {
     const x = await generateAlternativeHypotheses("The earth is flat",
@@ -8,3 +9,12 @@ test.only("test hypo", async () => {
     // x should be of length 3
     expect(x).toHaveLength(3);
 }, { timeout: 10000 });
+
+test.only("test post", async () => {
+    const x = await ky.post("https://critical-thinking.pages.dev/think", {
+        json: {
+            "hypothesis": "The earth is flat",
+            "context": "I live on the earth"
+        }}).json();
+    console.log(x);
+});
