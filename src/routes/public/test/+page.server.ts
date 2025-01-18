aeca98437587c2f5bd1f761cba5719115a2df815
eziox <ezioxxk@gmail.com>
2025-01-18 19:51:35 +0100

    init

--- src/routes/public/test/+page.server.ts
+++ src/routes/public/test/+page.server.ts
@@ -4,9 +4,9 @@ import {selectWord} from "$lib/server/db/queries";
 export const load = async (event) => {
     const db = event.locals.db;
     const w = "dessin"
-    const word = await selectWord(w, db);
-    if (!word) {
-        return json({error: "Word not found"});
-    }
-    return {word};
+    // const word = await selectWord(w, db);
+    // if (!word) {
+    //     return json({error: "Word not found"});
+    // }
+    // return {word};
 }
