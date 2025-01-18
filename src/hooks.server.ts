7491d258193d4d2e27c9477b82bb02cd69773beb
eziox <ezioxxk@gmail.com>
2025-01-18 21:19:56 +0100

    init

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,4 @@
 import {drizzle} from 'drizzle-orm/d1';
-import {ENVS, strEqIgnCase} from "$lib/client/common";
 import {SqlLiteClient} from "$lib/server/db/local/client";
 
 
@@ -11,12 +10,7 @@ export function handleError(input: any) {
 
 // const publicPaths = ['/public', '/docs', '/stripe', '/auth', '/cron', '/txt', '/blog','/books','/learn'];
 export const handle =  async ({ event, resolve }) => {
-    // if (strEqIgnCase(ENVS.DEV, )) {
-    //     event.locals.db = SqlLiteClient
-    //     return await resolve(event)
-    // }
     const DB = event.platform?.env?.DB;
-    if (!DB) throw new Error("DB is not set");
-    event.locals.db = drizzle(DB);
+    event.locals.db = DB ? drizzle(DB): SqlLiteClient;
     return await resolve(event)
 }
