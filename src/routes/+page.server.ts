14cac5b4b765159da81444fe638a97fcff193b02
eziox <ezioxxk@gmail.com>
2025-02-09 17:31:47 +0100

    try cache

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,9 +5,10 @@ export const load = async ({ locals, parent }) => {
     const userId = locals.userId;
     console.log("userId", userId, 'hypotheses', hypotheses.length);
     // if length > 0, take the id of the first hypothesis and redirect to /think/[id]
+    const timestamp = Date.now();
     if (hypotheses.length === 0) {
-        redirect(307, '/new');
+        redirect(307, '/new?timestamp=' + timestamp);
     } else {
-        redirect(307, `/${hypotheses[0].id}`);
+        redirect(307, `/${hypotheses[0].id}?timestamp=${timestamp}`);
     }
 }
